2a912a90fc7933317aff79b48ec8efcec007032a
hippo <hippo@example.com>
2024-06-01 18:21:16 +0800

    first commit

---
+++ words.js
@@ -0,0 +1,44 @@
+import plugin from '../../lib/plugins/plugin.js'
+import fetch from 'node-fetch'
+import { exec } from 'child_process';
+import common from "../../lib/common/common.js";
+import path from "path"
+
+const _path = path.join(process.cwd(), 'plugins/example')
+
+export class words extends plugin {
+    constructor () {
+		super({
+		  name: '文字狱',
+		  dsc: '抓取一些有趣的文字',
+		  event: 'message',
+		  priority: 3000,
+		  rule: [
+			{
+			  reg: '^#表白',
+			  fnc: 'tell'
+			},
+			{
+			  reg: '^.*?珊瑚宫.*$',
+			  fnc: 'revert'
+			}
+		  ]
+		})
+	}
+	
+	async tell(e) {
+		if (e.at) {
+			let num = Math.floor(100 + Math.random() * 899).toString()
+			e.reply([segment.at(e.ta), '我宣布，你是在我最无能为力的年纪，遇到的最想守护一生的第' + num + '个女孩子'])
+		}
+		
+		//let t = (await import('icqq' || 'oicq')).core
+		//logger.info(t || t.pb)
+	}
+	
+	async revert(e) {
+		if (e.isGroup)
+			e.group.recallMsg(e)
+	}
+	
+}
