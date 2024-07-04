0c724c9c30275d446bbf7fefb1b22ef62b00508c
hippo <hippo@example.com>
2024-07-04 22:16:16 +0800

    maintenance

--- words.js
+++ words.js
@@ -4,13 +4,13 @@ import { exec } from 'child_process';
 import common from "../../lib/common/common.js";
 import path from "path"
 
-const _path = path.join(process.cwd(), 'plugins/example')
+const _path = path.join(process.cwd(), 'plugins/wordwatcher')
 
 export class words extends plugin {
     constructor () {
 		super({
 		  name: '文字狱',
-		  dsc: '抓取一些有趣的文字',
+		  dsc: '屏蔽一些不合适的文字',
 		  event: 'message',
 		  priority: 3000,
 		  rule: [
@@ -28,17 +28,14 @@ export class words extends plugin {
 	
 	async tell(e) {
 		if (e.at) {
-			let num = Math.floor(100 + Math.random() * 899).toString()
-			e.reply([segment.at(e.ta), '我宣布，你是在我最无能为力的年纪，遇到的最想守护一生的第' + num + '个女孩子'])
+			let num = Math.floor(100 + Math.random() * 899)
+			e.reply([segment.at(e.ta), `我宣布，你是在我最无能为力的年纪，遇到的最想守护一生的第{$num}个女孩子`])
 		}
 		
-		//let t = (await import('icqq' || 'oicq')).core
-		//logger.info(t || t.pb)
 	}
 	
 	async revert(e) {
 		if (e.isGroup)
 			e.group.recallMsg(e)
 	}
-	
 }
